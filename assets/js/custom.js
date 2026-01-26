47b718ba878c715a03892b7ffbebf98f5bfc843e
David Wong <2597827+dvkwong@users.noreply.github.com>
2026-01-26 22:01:28 +1300

    Import mermaid

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,8 @@
+import mermaid from 'CDN/mermaid.esm.mjs';
 mermaid.registerIconPacks([
   {
-    name: 'logos', // Name to use in Mermaid syntax
-    loader: () => fetch('https://unpkg.com/@iconify-json/logos@1/icons.json').then((res) => res.json()),
+    name: 'logos',
+    loader: () =>
+      fetch('https://unpkg.com/@iconify-json/logos@1/icons.json').then((res) => res.json()),
   },
 ]);
